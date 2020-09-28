b6ef644507d911fb34623fbb9ac3debeaac083aa
Nishchay Arya <nishchay.arya92@gmail.com>
2020-09-28 17:01:53 -0400

    Set up database and install migrate

--- group_flask_starter/starter_app/app/__init__.py
+++ group_flask_starter/starter_app/app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, session
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-
+from flask_migrate import Migrate
 
 from .models import db, User
 from .api.user_routes import user_routes
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 db.init_app(app)
+migrate = Migrate(app, db)
 
 ## Application Security
 CORS(app)
--- group_flask_starter/starter_app/database.py
+++ group_flask_starter/starter_app/database.py
@@ -8,12 +8,12 @@ with app.app_context():
   db.drop_all()
   db.create_all()
 
-  ian = User(username = 'Ian', email = 'ian@aa.io')
-  javier = User(username = 'Javier', email = 'javier@aa.io')
-  dean = User(username = 'Dean', email = 'dean@aa.io')
-  angela = User(username = 'Angela', email = 'angela@aa.io')
-  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io')
-  alissa = User(username = 'Alissa', email = 'alissa@aa.io')
+  ian = User(username = 'Ian', email = 'ian@aa.io', hashed_password='password')
+  javier = User(username = 'Javier', email = 'javier@aa.io', hashed_password='password')
+  dean = User(username = 'Dean', email = 'dean@aa.io', hashed_password='password')
+  angela = User(username = 'Angela', email = 'angela@aa.io', hashed_password='password')
+  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io', hashed_password='password')
+  alissa = User(username = 'Alissa', email = 'alissa@aa.io', hashed_password='password')
 
   db.session.add(ian)
   db.session.add(javier)
