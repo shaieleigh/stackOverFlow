3a84e6e097704047ee9af48667cbda5d4567296b
Simon Carroll <snc40@georgetown.edu>
2020-09-28 12:59:06 -0400

    store folder

---
+++ group_flask_starter/client/src/store/auth.js
@@ -0,0 +1,48 @@
+import Cookies from 'js-cookie';
+
+const SET_USER = 'authentication/SET_USER';
+const REMOVE_USER = 'authentication/REMOVE_USER';
+
+
+export const setUser = (user) => {
+    return {
+        type: SET_USER,
+        user
+    }
+}
+
+export const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    }
+}
+
+export const signup = (email, password, username) => {
+    return async (dispatch) => {
+      const res = await fetch('api/users', {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "XSRF-TOKEN": Cookies.get("XSRF-TOKEN")
+        },
+        body: JSON.stringify({ email, password, username})
+      });
+      const data = await res.json();
+      debugger
+      dispatch(setUser(data));
+      res.data = data;
+      return res;
+    }
+  }
+
+
+export default function authReducer(state={}, action) {
+    switch(action.type) {
+      case SET_USER:
+        return action.user;
+      case REMOVE_USER:
+        return {};
+      default:
+        return state;
+    }
+  }
