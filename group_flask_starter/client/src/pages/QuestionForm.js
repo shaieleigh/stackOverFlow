9b42f716492aa5b0d19bc4f355c5bc0205cce9d1
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-01 19:01:42 -0400

    Set up question form react component

--- group_flask_starter/client/src/pages/QuestionForm.js
+++ group_flask_starter/client/src/pages/QuestionForm.js
@@ -1,28 +1,43 @@
-import { Input } from '@material-ui/core';
-import React, { Component } from 'react'
+import React from 'react'
+import { Redirect } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import './QuestionForm.css'
 
-export class QuestionForm extends Component {
-    render() {
-        return (
-            <>
-                <NavBar />
-                <div id='form-container'>
-                    <form id='question-form'>
-                        <label className='title-divs'>Title</label>
-                        <input className='input-question-form' placeholder='e.g. Is there an R function for finding the index of an element in a vector?' />
-                        <label className='title-divs'>Body</label>
-                        <textarea className='input-question-form' rows='12' />
-                        <label className='title-divs'>Tags</label>
-                        <input className='input-question-form' placeholder='e.g. (python javascript)' />
-                    </form>
-                    <button type='submit' className='question-form-button'>Ask this question</button>
-                </div>
-            </>
-        )
+function QuestionForm1() {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [tags, setTags] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await fetch('/api/questions/ask', {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+                "XSRF-TOKEN": Cookies.get("XSRF-TOKEN")
+            },
+            body: JSON.stringify({ title, body })
+        });
+        <Redirect to='/questions' />
     }
+
+    return (
+        <>
+            <NavBar />
+            <div id='form-container'>
+                <form id='question-form'>
+                    <label className='title-divs'>Title</label>
+                    <input className='input-question-form' placeholder='e.g. Is there an R function for finding the index of an element in a vector?' onChange={e => setTitle(e.target.value)} />
+                    <label className='title-divs'>Body</label>
+                    <textarea className='input-question-form' rows='12' onChange={e => setBody(e.target.value)} />
+                    <label className='title-divs'>Tags</label>
+                    <input className='input-question-form' placeholder='e.g. (python javascript)' onChange={e => setTags(e.target.value)} />
+                </form>
+                <button type='submit' className='question-form-button' onClick={handleSubmit}>Ask this question</button>
+            </div>
+        </>
+    )
 }
 
-export default QuestionForm
-
+export default QuestionForm1
