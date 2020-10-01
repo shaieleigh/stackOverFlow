e86533797f2bbae62076fe99a9ec4af3fdf08270
Nishchay Arya <nishchay.arya92@gmail.com>
2020-10-01 16:13:56 -0400

    question form css

--- group_flask_starter/client/src/pages/QuestionForm.js
+++ group_flask_starter/client/src/pages/QuestionForm.js
@@ -11,12 +11,13 @@ export class QuestionForm extends Component {
                 <div id='form-container'>
                     <form id='question-form'>
                         <label className='title-divs'>Title</label>
-                        <input className='input-question-form' />
+                        <input className='input-question-form' placeholder='e.g. Is there an R function for finding the index of an element in a vector?' />
                         <label className='title-divs'>Body</label>
-                        <textarea className='input-question-form' rows='8' />
+                        <textarea className='input-question-form' rows='12' />
                         <label className='title-divs'>Tags</label>
-                        <input className='input-question-form' />
+                        <input className='input-question-form' placeholder='e.g. (python javascript)' />
                     </form>
+                    <button type='submit' className='question-form-button'>Ask this question</button>
                 </div>
             </>
         )
