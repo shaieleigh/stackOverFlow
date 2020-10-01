0357d8fb72656a9f0f284b98ecdb91310e3ecdc5
Lauren <elle.rachel.writes@gmail.com>
2020-10-01 10:47:02 -1000

    simon fixed logo and working on heroku

---
+++ group_flask_starter/starter_app/app/static/precache-manifest.cfcec1f27d31775de015ac4264de37a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22d2304795a9537d2c4275955ad6e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a8603d1384e4c9adf30",
+    "url": "/static/css/main.dfef43e4.chunk.css"
+  },
+  {
+    "revision": "c50a85b48ed168b6dd47",
+    "url": "/static/js/2.0e468dd7.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/static/js/2.0e468dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8603d1384e4c9adf30",
+    "url": "/static/js/main.38a98e2e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4248a20c004f1d4afc660e202f5c0251",
+    "url": "/static/media/snackoverflowlogo.4248a20c.png"
+  }
+]);
