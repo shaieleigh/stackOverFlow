e02b5f84f1441aae3b1fb73d699726495600b0c5
djicegiggles <shaieleigh1@gmail.com>
2020-10-02 17:35:07 -0400

    more clean up

--- group_flask_starter/client/src/App.js
+++ group_flask_starter/client/src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
-// { useState, useEffect }
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './pages/Pages';
-// import { useDispatch } from 'react-redux';
-// import { setUser } from './store/auth';
+import { useDispatch } from 'react-redux';
+import { setUser } from './store/auth';
 import { CssBaseline } from '@material-ui/core';
 // import ThemeProvider from 'material-ui/styles/';
 
